b2d8cc863bd7a5d511f546868b6aab182978d0e7
FilipeGundim <filipegundim@outlook.com>
2019-09-05 17:05:43 -0300

    criando aplicattion

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <div>
+            <footer className="footer">
+                JGCampos999 | FilipeGundim
+                © - All Rights Reserveds
+            </footer>
+        </div>
+    )
+}
+
+export default footer;
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div>
+            <header className="Header">
+                Paulistão 2019
+            </header>
+        </div>
+    )
+}
+
+export default header;
