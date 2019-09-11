964ed97f6fe0010395c4329bc981fc6e75a09970
FilipeGundim <filipegundim@outlook.com>
2019-09-11 16:38:30 -0300

    rs

---
+++ frontend/src/components/route.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+const Route = () =>  {
+    return(
+        <Router>
+            <Route/>
+        </Router>
+    )
+}
